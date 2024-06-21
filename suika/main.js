dc5317d51066ee9e26dbc72d001c556403e45ef9
vkdnj12 <teuha688@gmail.com>
2024-06-21 09:49:33 +0900

    Key

--- suika/main.js
+++ suika/main.js
@@ -4,7 +4,8 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    World = Matter.World;
+    World = Matter.World,
+    Body = Matter.Body;
 
 // 엔진 선언
 const engine = Engine.create();
@@ -44,6 +45,7 @@ const ground = Bodies.rectangle(310, 820, 620, 60, {
 // 경계선 생성
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic : true, // 고정해주는 기능
+    isSensor : true, // 충돌은 감지하나 물리엔진은 적용 안함
     render : { fillStyle : '#E6B143'} // 색상 지정
 })
 
@@ -53,20 +55,67 @@ World.add(world, [leftWall, rightWall, ground, topLine]);
 Render.run(render);
 Runner.run(engine);
 
+// 현재 과일 값을 저장할 변수 생성
+let currentBody = null;
+let currentFruit = null;
+// 키 조작을 제어하는 변수 생성
+let disableAction = false;
+
 // 과일 떨어지는 함수 만들기
 function addFruit(){
 
     // 과일 index 저장
-    const index = 0;
+    const index = Math.floor(Math.random() * 5);
 
     const fruit = FRUITS[index];
 
     const body = Bodies.circle(300, 50, fruit.radius, {
+        index : index,
+        isSleeping : true,
         render : {
-            sprite : { texture : `${fruit.name}.png`}
-        }
+            sprite : { texture : `${fruit.name}.png`},
+                        // texture : fruit.name + '.png' },
+        },
+        restitution : 0.2,
     });
-    World.add(world, body)
+
+    // 현재 과일값 저장
+    currentBody = body;
+    currentFruit = fruit;
+
+    World.add(world, body);
+}
+
+window.onkeydown = (event) => {
+
+    // 제어 조작 변수가 true 경우 바로 리턴
+    if (disableAction)
+        return;
+
+    switch(event.code){
+        case "KeyA":
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x - 10,
+                y: currentBody.position.y
+            })
+            break;
+        case "KeyD":
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x + 10,
+                y: currentBody.position.y
+            })
+            break;    
+        case "KeyS":
+            currentBody.isSleeping = false;
+
+            disableAction = true;
+
+            setTimeout(() => {
+                addFruit();
+                disableAction = false;
+            }, 1000);
+            break;    
+    }
 }
 
 addFruit();
